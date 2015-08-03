38aacaf488be3d1bcb0b0edc9bab197e630c31c8
deadbok <martin.groenholdt@gmail.com>
2015-08-03 14:25:07 +0200

    Logging and REST.

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,22 @@
 '''
-Created on 01/08/2015
-
-@author: oblivion
+:since: 01/08/2015
+:author: oblivion
 '''
+import logging
+
 from flask import Flask
 
-app = Flask(__name__)
-app.config.from_object('config')
+from app.log import logger, init_file_log, init_console_log, close_log
 
-from app import views
+
+APP = Flask(__name__)
+APP.config.from_object('config')
+
+if APP.config['DEBUG']:
+    init_console_log(logging.DEBUG)
+    init_file_log(logging.DEBUG)
+else:
+    init_console_log(logging.CRITICAL)
+    init_file_log(logging.WARNING)
+
+from app import views  #@NoMove
--- app/views.py
+++ app/views.py
@@ -1,9 +1,7 @@
 '''
-Created on 01/08/2015
-
-@author: oblivion
+:since: 01/08/2015
+:author: oblivion
 '''
-
 from datetime import datetime
 from socket import getfqdn
 
@@ -11,101 +9,184 @@ from flask import jsonify
 from flask import render_template
 import psutil
 
-from app import app
+from app import APP
 
-avg_rcv_speed = 0
-avgSendSpeed = 0
-lastRcvBytes = 0
-lastSendBytes = 0
-lastRcvTime = 0
-lastSendTime = 0
+from app.log import logger
 
-@app.route('/')
-@app.route('/index')
+AVG_RCV_SPEED = 0
+AVG_SEND_SPEED = 0
+LAST_RCV_BYTES = 0
+LAST_SEND_BYTES = 0
+LAST_RCV_TIME = 0
+LAST_SEND_TIME = 0
+
+
+@APP.route('/', methods=['GET'])
+@APP.route('/index', methods=['GET'])
 def index():
-    uptime = 0
-
-# Find the lighttpd process and get the create time, to calculate the up time.
-    for process in psutil.process_iter():
-        if (process.name().find(app.config['PROCESS_NAME']) != -1):
-            uptime = (datetime.now() -
-                      datetime.fromtimestamp(process.create_time()))
-    uptime = str(uptime).split('.')[0]
-
-# Get latest remote address from access.log.
-    lines = list()
-    try:
-        with open(app.config['ACCESS_LOG'], "rt") as log_file:
-# And so lets waste a lot of memory, said the programmer.
-            lines = log_file.readlines()
-        log_file.close()
-    except PermissionError:
-        lines.append("Permission denied reading server log file.")
-
-    remote = lines[-1].split(' ')[0]
-# Set total requests
-    accesses = len(lines)
-
-    return render_template('index.html', hostname=getfqdn(),
-                           url=remote, uptime=uptime, accesses=accesses)
+    '''
+    Render the template for the dash board.
+    '''
+    logger.debug("Rendering dash board template.")
+    return render_template('index.html', hostname=getfqdn())
 
 
-@app.route('/rest/connections')
+@APP.route('/rest/connections', methods=['GET'])
 def connections():
+    '''
+    Return a JSON object with number of connections on a port.
+    '''
+    logger.debug("Getting active connections.")
     active = 0
-# Get connections on port 80
+    # Get connections on port 80
     conn = psutil.net_connections('inet')
     for connection in conn:
-        if (connection.laddr[1] == 80):
+        if connection.laddr[1] == APP.config['PORT']:
             active += 1
-#Return JSON
+    # Return JSON
+    logger.debug("Connections: " + str(active))
     return jsonify(connections=active)
 
 
-@app.route('/rest/recieve_speed')
+@APP.route('/rest/receive_speed', methods=['GET'])
 def rcv_speed():
-    global avg_rcv_speed
-    global lastRcvBytes
-    global lastRcvTime
+    '''
+    Return average speed during the last 2 calls in JSON.
+    '''
+    global AVG_RCV_SPEED
+    global LAST_RCV_BYTES
+    global LAST_RCV_TIME
 
-    interfaces = psutil.net_io_counters(True)
-    now = datetime.now()
-    s = interfaces[app.config['INTERFACE']].bytes_recv
+    logger.debug("Get average incoming speed.")
 
-    if lastRcvTime == 0:
-        lastRcvBytes = s
-        lastRcvTime = now
-        return jsonify(speed=0)
+    try:
+        interfaces = psutil.net_io_counters(True)
+        now = datetime.now()
+        total_bytes_recv = interfaces[APP.config['INTERFACE']].bytes_recv
 
-    time = (now - lastRcvTime).seconds
-    rcv_bytes = s - lastRcvBytes
-    speed = (rcv_bytes / time) / 1024
+        if LAST_RCV_TIME == 0:
+            LAST_RCV_BYTES = total_bytes_recv
+            LAST_RCV_TIME = now
+            logger.debug("First run, no average yet.")
+            return jsonify(speed=0)
 
-    avg_rcv_speed = (avg_rcv_speed + speed) / 2
-    lastRcvBytes = s
+        time = (now - LAST_RCV_TIME).seconds
+        logger.debug("Sample period: " + str(time) + " seconds.")
+        rcv_bytes = total_bytes_recv - LAST_RCV_BYTES
+        logger.debug("Bytes received: " + str(rcv_bytes) + " bytes.")
+        speed = (rcv_bytes / time) / 1024
+        logger.debug("Sampled speed: " + str(speed) + "KiB/s.")
 
-    return jsonify(speed="{0:.2f}".format(avg_rcv_speed))
+        AVG_RCV_SPEED = (AVG_RCV_SPEED + speed) / 2
+        logger.debug("Average speed: " + str(AVG_RCV_SPEED) + " KiB/s.")
+        LAST_RCV_BYTES = total_bytes_recv
+        LAST_RCV_TIME = now
+    except ZeroDivisionError:
+        logger.warning("Sampling to fast, while sampling incoming speed.")
 
-@app.route('/rest/send_speed')
+    return jsonify(speed="{0:.2f}".format(AVG_RCV_SPEED))
+
+
+@APP.route('/rest/send_speed', methods=['GET'])
 def send_speed():
-    global avgSendSpeed
-    global lastSendBytes
-    global lastSendTime
+    '''
+    Return average speed during the last 2 calls in JSON.
+    '''
+    global AVG_SEND_SPEED
+    global LAST_SEND_BYTES
+    global LAST_SEND_TIME
 
-    interfaces = psutil.net_io_counters(True)
-    now = datetime.now()
-    s = interfaces[app.config['INTERFACE']].bytes_sent
+    logger.debug("Get average outgoing speed.")
 
-    if lastSendTime == 0:
-        lastSendBytes = s
-        lastSendTime = now
-        return jsonify(speed=0)
+    try:
+        interfaces = psutil.net_io_counters(True)
+        now = datetime.now()
+        total_bytes_sent = interfaces[APP.config['INTERFACE']].bytes_sent
 
-    time = (now - lastSendTime).seconds
-    send_bytes = s - lastSendBytes
-    speed = (send_bytes / time) / 1024
+        if LAST_SEND_TIME == 0:
+            LAST_SEND_BYTES = total_bytes_sent
+            LAST_SEND_TIME = now
+            logger.debug("First run, no average yet.")
+            return jsonify(speed=0)
 
-    avgSendSpeed = (avgSendSpeed + speed) / 2
-    lastSendBytes = s
+        time = (now - LAST_SEND_TIME).seconds
+        logger.debug("Sample period: " + str(time) + " seconds.")
+        sent_bytes = total_bytes_sent - LAST_SEND_BYTES
+        logger.debug("Bytes sent: " + str(sent_bytes) + " bytes.")
+        speed = (sent_bytes / time) / 1024
+        logger.debug("Sampled speed: " + str(speed) + "KiB/s.")
 
-    return jsonify(speed="{0:.2f}".format(avgSendSpeed))
+        AVG_SEND_SPEED = (AVG_SEND_SPEED + speed) / 2
+        logger.debug("Average speed: " + str(AVG_SEND_SPEED) + " KiB/s.")
+        LAST_SEND_BYTES = total_bytes_sent
+        LAST_SEND_TIME = now
+    except ZeroDivisionError:
+        logger.warning("Sampling to fast, while sampling outgoing speed.")
+
+    return jsonify(speed="{0:.2f}".format(AVG_SEND_SPEED))
+
+
+@APP.route('/rest/uptime', methods=['GET'])
+def uptime():
+    '''
+    Return uptime of the server process.
+    '''
+    logger.debug('Getting up time for "' + APP.config['PROCESS_NAME'] + '".')
+    proc_time = 0
+
+    # Find the lighttpd process and get the create time, to calculate the up
+    # time.
+    for process in psutil.process_iter():
+        if process.name().find(APP.config['PROCESS_NAME']) != -1:
+            logger.debug("Process found.")
+            proc_time = (datetime.now() -
+                         datetime.fromtimestamp(process.create_time()))
+    logger.debug("Up time: " + str(proc_time) + ".")
+    proc_time = str(proc_time).split('.')[0]
+
+    return jsonify(uptime=proc_time)
+
+
+@APP.route('/rest/remote_host', methods=['GET'])
+def remote_host():
+    '''
+    Return name of the remote host.
+    '''
+    logger.debug("Getting last remote host from access log.")
+    # Get latest remote address from access.log.
+    lines = list()
+    try:
+        with open(APP.config['ACCESS_LOG'], "rt") as log_file:
+            # And so lets waste a lot of memory, said the programmer.
+            lines = log_file.readlines()
+        log_file.close()
+    except PermissionError:
+        logger.error("Permission denied reading log file: " +
+                     APP.config['ACCESS_LOG'])
+        lines.append("Permission denied reading server log file.")
+
+    logger.debug("Log line: " + lines[-1] + ".")
+
+    return jsonify(address=lines[-1].split(' ')[0])
+
+
+@APP.route('/rest/accesses', methods=['GET'])
+def accesses():
+    '''
+    Return number of accesses logged..
+    '''
+    # Get latest remote address from access.log.
+    lines = list()
+    try:
+        with open(APP.config['ACCESS_LOG'], "rt") as log_file:
+            # And so lets waste a lot of memory, said the programmer.
+            lines = log_file.readlines()
+        log_file.close()
+    except PermissionError:
+        logger.error("Permission denied reading log file: " +
+                     APP.config['ACCESS_LOG'])
+        lines.append("Permission denied reading server log file.")
+
+    logger.debug("Log line: " + lines[-1] + ".")
+
+    return jsonify(accesses=len(lines))
