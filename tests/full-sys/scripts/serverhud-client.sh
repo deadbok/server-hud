2af1780906c8bfbc662c47c0ba26ac1581fc6eaf
deadbok <martin.groenholdt@gmail.com>
2017-12-05 08:40:23 +0100

    Stuff I did last

--- tests/full-sys/scripts/serverhud-client.sh
+++ tests/full-sys/scripts/serverhud-client.sh
@@ -2,7 +2,6 @@
 
 cp /home/vagrant/config.py /home/serverhud/config.py
 cp /home/vagrant/serverhud-client.service /lib/systemd/system/.
-cp /home/vagrant/serverhud-client.service /lib/systemd/system/.
 chmod 664 /lib/systemd/system/serverhud-client.service
 systemctl daemon-reload
 systemctl enable serverhud-client.service
--- tests/full-sys/scripts/webserver.sh
+++ tests/full-sys/scripts/webserver.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-apt-get install -y lighttpd
+apt-get install -y lighttpd httperf
 gpasswd -a serverhud www-data
+lighty-enable-mod accesslog
+service lighttpd force-reload
 systemctl enable lighttpd
 service lighttpd start
